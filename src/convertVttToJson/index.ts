84b8f1b55023c9b4b37145f86ab2626c49cfe1d8
Alexey Berezin <alexey.berezin@joyn.de>
2021-04-19 23:06:42 +0200

    fix: 🧪 convertVttToJson types

--- src/convertVttToJson/index.ts
+++ src/convertVttToJson/index.ts
@@ -1,3 +1,4 @@
+export function convertVttToJson(vttString: string): Promise<any[]>;
 export function convertVttToJson(vttString) {
   return new Promise((resolve, reject) => {
     var current = {};
@@ -10,14 +11,17 @@ export function convertVttToJson(vttString) {
       } else if (line.indexOf("-->") !== -1) {
         start = true;
 
+        // @ts-expect-error
         if (current.start) {
           sections.push(clone(current));
         }
 
         current = {
+          // @ts-expect-error
           start: timeString2ms(
             line.split("-->")[0].trimRight().split(" ").pop()
           ),
+          // @ts-expect-error
           end: timeString2ms(
             line.split("-->")[1].trimLeft().split(" ").shift()
           ),
@@ -35,9 +39,12 @@ export function convertVttToJson(vttString) {
               ) === line.replace(/<\/?[^>]+(>|$)/g, "")
             ) {
             } else {
+              // @ts-expect-error
               if (current.part.length === 0) {
+                // @ts-expect-error
                 current.part = line;
               } else {
+                // @ts-expect-error
                 current.part = `${current.part} ${line}`;
               }
               // If it's the last line of the subtitles
@@ -46,8 +53,10 @@ export function convertVttToJson(vttString) {
               }
             }
           } else {
+            // @ts-expect-error
             current.part = line;
             sections.push(clone(current));
+            // @ts-expect-error
             current.part = "";
           }
         }
@@ -58,7 +67,7 @@ export function convertVttToJson(vttString) {
 
     var regex = /(<([0-9:.>]+)>)/gi;
     sections.forEach((section) => {
-      strs = section.part.split();
+      const strs = section.part.split();
       var results = strs.map(function (s) {
         return s.replace(regex, function (n) {
           return n.split("").reduce(function (s, i) {
@@ -66,9 +75,9 @@ export function convertVttToJson(vttString) {
           }, 0);
         });
       });
-      cleanText = results[0].replace(/<\/?[^>]+(>|$)/g, "");
-      cleanArray = cleanText.split(" ");
-      resultsArray = [];
+      const cleanText = results[0].replace(/<\/?[^>]+(>|$)/g, "");
+      const cleanArray = cleanText.split(" ");
+      const resultsArray = [];
       cleanArray.forEach(function (item) {
         if (item.indexOf("==") > -1) {
           var pair = item.split("==");
@@ -79,12 +88,12 @@ export function convertVttToJson(vttString) {
           }
           resultsArray.push({
             word: cleanWord(item.split("==")[0]),
+            // @ts-expect-error
             time: timeString2ms(item.split("==")[1]),
           });
         } else {
           resultsArray.push({
             word: cleanWord(item),
-            time: undefined,
           });
         }
       });
